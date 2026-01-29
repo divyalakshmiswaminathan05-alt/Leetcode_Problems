638dfd3aec39e417b972f321b9bc07d268febb2a
Divya Lakshmi S <divyalakshmiswaminathan05@gmail.com>
2026-01-29 23:42:43 +0530

    Time: 0 ms (100%), Space: 8.3 MB (100%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,11 @@
+int reverse(int x){
+    long long int rev=0;
+    while(x!=0){
+    int digit=x%10;
+    rev=(rev*10)+digit;
+    x=x/10;
+    }
+    if(rev<INT_MIN || rev>INT_MAX)
+    return 0;
+    return (int)rev;
+}
