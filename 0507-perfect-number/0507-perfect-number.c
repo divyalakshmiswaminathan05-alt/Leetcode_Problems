fae728086e952775f4e1193784451c971f97bb2a
Divya Lakshmi S <divyalakshmiswaminathan05@gmail.com>
2026-03-18 10:02:17 +0530

    Time: 0 ms (100%), Space: 8.4 MB (44.54%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.c
+++ 0507-perfect-number/0507-perfect-number.c
@@ -1,10 +1,18 @@
-bool checkPerfectNumber(int num) {
-    int sum=0;
-    for(int i=1;i<=num/2;i++){
-        if(num%i==0)
-            sum=sum+i;
-    }
-    if(sum==num)
-    return true;
-    else return false;
+bool checkPerfectNumber(int n) {
+    int sum=1;
+    for(int i=2;i<=n/i;i++){
+            if(n%i==0){
+                if(i==n/i)
+                    sum=sum+i;
+                else
+                    sum=sum+i+(n/i);
+            }
+           
+ }
+ if (n==1)
+ sum=0;
+  if (sum==n)
+            return true;
+            else
+            return false;
 }
