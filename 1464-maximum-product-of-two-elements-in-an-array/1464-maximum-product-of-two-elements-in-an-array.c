89049ffa339140b124410f750072685a8a647bba
Divya Lakshmi S <divyalakshmiswaminathan05@gmail.com>
2026-03-25 10:53:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.c
@@ -0,0 +1,13 @@
+int maxProduct(int* nums, int numsSize) {
+    int max1=nums[0],max2=0;
+    for(int i=0;i<numsSize;i++){
+        if(max1<nums[i]){
+            max2=max1;
+            max1=nums[i];
+        }
+        else if(max2<nums[i]){
+            max2=nums[i];
+        }
+    }
+    return (max1-1)*(max2-1);
+}
