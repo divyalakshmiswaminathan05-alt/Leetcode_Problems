2df950adbfcab38c206c3a6f0c30e2f263fe5454
Divya Lakshmi S <divyalakshmiswaminathan05@gmail.com>
2026-08-24 12:02:41 +0530

    Time: 68 ms (41.38%), Space: 23.5 MB (11.62%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
@@ -5,16 +5,35 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-    while(headB!=NULL){
-        struct ListNode *temp=headA;
-        while(temp!=NULL){
-            if(temp==headB)
-            return headB;
-            temp=temp->next;
-        }
-        headB=headB->next;
+struct ListNode *getIntersectionNode(struct ListNode *head1, struct ListNode *head2) {
+int l1,l2;
+l1=l2=0;
+struct ListNode *p1,*p2;
+p1=head1;
+p2=head2;
+while(p1!=NULL){
+    l1++;
+    p1=p1->next;
+}
+while(p2!=NULL){
+    l2++;
+    p2=p2->next;
+}
+p1=head1;
+p2=head2;
+if(l1>l2){
+    for(int i=0;i<l1-l2;i++){
+        p1=p1->next;
     }
-    return NULL;
-    
+}
+else{
+    for(int i=0;i<l2-l1;i++){
+        p2=p2->next;
+    }
+}
+while(p1!=p2){
+    p1=p1->next;
+    p2=p2->next;
+}
+    return p1;
 }
